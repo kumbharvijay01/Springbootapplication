ead6431f7e91c868b8923da98ed966f8402187ad
kumbharvijay01 <kumbharvijay01@gmail.com>
2021-06-11 18:55:03 +0530

    First Commit
    First Commit

---
+++ HelloWorld/src/org/test/helloworld/TestClass.java
@@ -0,0 +1,10 @@
+package org.test.helloworld;
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello GIT world");
+	}
+
+}
