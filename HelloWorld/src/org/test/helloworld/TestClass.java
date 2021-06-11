954299441cd51302299214004d6667bfb20eabc4
kumbharvijay01 <kumbharvijay01@gmail.com>
2021-06-11 19:29:49 +0530

    Comment Removed

--- HelloWorld/src/org/test/helloworld/TestClass.java
+++ HelloWorld/src/org/test/helloworld/TestClass.java
@@ -3,8 +3,8 @@ package org.test.helloworld;
 public class TestClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("Hello GIT world");
+		
 	}
 
 }
