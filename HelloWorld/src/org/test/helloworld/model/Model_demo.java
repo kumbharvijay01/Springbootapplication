2b645804a893b94b14b713cf59967cb0061f95aa
kumbharvijay01 <kumbharvijay01@gmail.com>
2021-06-11 19:41:38 +0530

    Some Changes

--- HelloWorld/src/org/test/helloworld/model/Model_demo.java
+++ HelloWorld/src/org/test/helloworld/model/Model_demo.java
@@ -3,7 +3,7 @@ package org.test.helloworld.model;
 public class Model_demo {
 
 	public static void main(String[] args) {
-System.out.println("Model Demo");
+			System.out.println("Model Demo");
 		System.out.println("Changes Done on Git Hub");
 		
 	}
